0c08326aba0a6a9942b5d086404b4627d827cd91
Tony Bo <bochuangjie@gmail.com>
2020-12-26 20:52:07 +0800

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA3QaRFYbRs4qmjjrANXQU-saWM6TwH-no",
+    authDomain: "clone-a04c2.firebaseapp.com",
+    projectId: "clone-a04c2",
+    storageBucket: "clone-a04c2.appspot.com",
+    messagingSenderId: "16595804788",
+    appId: "1:16595804788:web:ba74675199be915a19e721",
+    measurementId: "G-N2JJM7B9ZP"
+  };
